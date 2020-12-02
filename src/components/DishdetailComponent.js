68ec2c7bb3b88768576daeef7aa92447295cf78e
42Bree <seongjinchang@42maru.com>
2020-12-02 18:28:25 +0900

    Start Assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,63 @@
-import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, {Component} from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem,Modal, ModalHeader, ModalBody, Form, FormGroup, Input, Label, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
+
+
+class CommentForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false
+        }; 
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+    }
+    handleSubmit(values) {
+        console.log('Current State is: ' + JSON.stringify(values));
+        alert('Current State is: ' + JSON.stringify(values));
+        // event.preventDefault();
+    }
+    render() {
+        return(
+            <div>
+                <Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span>Submit Comment</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+                    <ModalBody>
+                    <Form onSubmit={this.handleLogin}>
+                            <FormGroup>
+                                <Label htmlFor="username">Username</Label>
+                                <Input type="text" id="username" name="username"
+                                    innerRef={(input) => this.username = input} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label htmlFor="password">Password</Label>
+                                <Input type="password" id="password" name="password"
+                                    innerRef={(input) => this.password = input}  />
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check>
+                                    <Input type="checkbox" name="remember"
+                                    innerRef={(input) => this.remember = input}  />
+                                    Remember me
+                                </Label>
+                            </FormGroup>
+                            <Button type="submit" value="submit" color="primary">Login</Button>
+                        </Form>
+                    </ModalBody>
+                </Modal>
+            </div>
+        );
+    }
+}
+
 
 function RenderDish({dish}) {
 
@@ -23,31 +79,49 @@ function RenderDish({dish}) {
         );
 }
 
+function RenderComments(comments) {
+    let commentList = comments.comments.map((comment, i) => (
+      <li key={i} className="commentList">
+        {comment.comment}
+        <br />
+        <br />
+        -- {comment.author},
+        {new Intl.DateTimeFormat("en-US", {
+          year: "numeric",
+          month: "short",
+          day: "2-digit"
+        }).format(new Date(Date.parse(comment.date)))}
+        <br />
+        <br />
+      </li>
+    ));
+    commentList.push(<CommentForm></CommentForm>);
+    return commentList;
+  }
+// function RenderComments({comments}) {
 
-function RenderComments({comments}) {
+//     if (comments == null)
+//         return(
+//             <div></div>
+//         )
 
-    if (comments == null)
-        return(
-            <div></div>
-        )
-
-    const dishComment = comments.map((comment) => {
-        return (
-            <li key={comment.id} className="m-1">
-                <p>{comment.comment}</p>
-                <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-            </li>
-        )
-    });
-    return(
-        <div className="col-12 col-md-5 m-1">
-            <h4>Comments</h4>
-            <ul className="list-unstyled">
-                {dishComment}
-            </ul>
-        </div>
-    )
-}
+//     const dishComment = comments.map((comment) => {
+//         return (
+//             <li key={comment.id} className="m-1">
+//                 <p>{comment.comment}</p>
+//                 <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+//             </li>
+//         )
+//     });
+//     return(
+//         <div className="col-12 col-md-5 m-1">
+//             <h4>Comments</h4>
+//             <ul className="list-unstyled">
+//                 {dishComment}
+//             </ul>
+//         </div>
+//     )
+// }
 
 const DishDetail = (props) => {
     if (props.dish!=null)
