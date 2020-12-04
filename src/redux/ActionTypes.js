bd4ca0385e163ba087ff5ead1a3278903a6b4ff4
42Bree <seongjinchang@42maru.com>
2020-12-04 13:37:15 +0900

    Fetch from Server

--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -2,3 +2,12 @@ export const ADD_COMMENT = 'ADD_COMMENT';
 export const DISHES_LOADING = 'DISHES_LOADING';
 export const DISHES_FAILED = 'DISHES_FAILED';
 export const ADD_DISHES = 'ADD_DISHES';
+
+export const ADD_COMMENTS = 'ADD_COMMENTS';
+//comment will be loaded behind the scenes
+// when we render our application, we'll first render the home component.
+// By the time the home component is rendered, the comments will also be fetched in. That's why no coment loading here.
+export const COMMENTS_FAILED = 'COMMENTS_FAILED';
+export const PROMOS_LOADING = 'PROMOS_LOADING';
+export const ADD_PROMOS = 'ADD_PROMOS';
+export const PROMOS_FAILED = 'PROMOS_FAILED';
